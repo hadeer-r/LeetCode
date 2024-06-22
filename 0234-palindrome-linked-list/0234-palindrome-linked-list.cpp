81652836b69d31b2b90e919499fdf4037ad76d6c
Hadeer Ramadan <147225434+hadeer-r@users.noreply.github.com>
2024-06-22 11:48:09 +0300

    Time: 180 ms (38.35%), Space: 123.2 MB (37.61%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,19 +10,34 @@
  */
 class Solution {
 public:
-    bool isPalindromeText(string str){
-        int sz=(int)str.size();
-        for(int i=0;i<sz/2;i++){
-            if(str[i]!=str[sz-i-1])
-            return false;
+    bool isPalindrome(ListNode* head) {
+        bool odd{};
+        stack<int> half;
+        ListNode *p1 = head, *p2 = head;
+        for (;; p1 = p1->next, p2 = p2->next->next) {
+            half.push(p1->val);
+            if (!p2)
+                break;
+            if (!p2->next) {
+                odd = true;
+                break;
+            }
+        }
+        if (!odd) {
+            int tmp = half.top();
+            half.pop();
+            if (half.top() == tmp) {
+                half.pop();
+            } else
+                return false;
+        } else {
+            half.pop();
+        }
+        for (ListNode* cur = p1->next; cur; cur = cur->next) {
+            if (cur->val != half.top())
+                return false;
+            half.pop();
         }
         return true;
-    }
-    bool isPalindrome(ListNode* head) {
-        string str{""};
-        for(ListNode*cur=head;cur;cur=cur->next){
-            str+=(char)(cur->val+'0');
-        }
-        return isPalindromeText(str);
     }
 };
