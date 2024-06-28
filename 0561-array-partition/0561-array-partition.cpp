05766ec10fec7d55f03fd3edc7b9f1e7f48ae937
Hadeer Ramadan <147225434+hadeer-r@users.noreply.github.com>
2024-06-28 06:33:27 +0300

    Time: 211 ms (6.48%), Space: 94.1 MB (6.79%) - LeetHub

--- 0561-array-partition/0561-array-partition.cpp
+++ 0561-array-partition/0561-array-partition.cpp
@@ -1,8 +1,28 @@
 class Solution {
 public:
+    void sortArray(vector<int> &nums) {
+        int max_val = nums[0],min_val=nums[0], sz = nums.size();
+        for (int i = 0; i < sz; i++) {
+            if (max_val < nums[i])
+                max_val = nums[i];
+            if(min_val>nums[i])
+                min_val=nums[i];
+        }
+
+        unordered_map<int,int> freq;
+        for(int i=0;i<sz;i++) {
+            freq[nums[i]]++;
+        }
+        int idx{};
+        for (int i=min_val;i<=max_val;i++) {
+            for(int j=0;j<freq[i];j++,idx++) {
+                nums[idx]=i;
+            }
+        }
+    }
     int arrayPairSum(vector<int>& nums) {
         int max_sum{};
-        sort(nums.begin(),nums.end());
+        sortArray(nums);
         for(int i=0;i<(int)nums.size();i+=2){
             max_sum+=nums[i];
         }
