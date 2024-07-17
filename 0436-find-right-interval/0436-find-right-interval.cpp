0b8114d5b8f79dafaa0b2fead6ac5bfe62eeb6c1
Hadeer Ramadan <147225434+hadeer-r@users.noreply.github.com>
2024-07-17 19:25:10 +0300

    Time: 56 ms (50.6%), Space: 31.7 MB (47.81%) - LeetHub

--- 0436-find-right-interval/0436-find-right-interval.cpp
+++ 0436-find-right-interval/0436-find-right-interval.cpp
@@ -1,39 +1,16 @@
 class Solution {
 public:
-    vector<int> search(vector<vector<int>> &v, int target) {
-        int start = 0, end = (int) v.size() - 1;
-
-        while (start <= end) {
-            int middle = start + (end - start) / 2;
-            if (v[middle][0] == target)
-                return v[middle];
-            if(v[middle][0]>target&& middle!=0&&v[middle-1][0]<target)
-                return v[middle];
-            else if(v[middle][0]>target)
-                end =middle-1;
-            else
-                start=middle+1;
+    vector<int> findRightInterval(vector<vector<int>> &intervals) {
+        int sz=(int) intervals.size();
+        vector<int> result(sz);
+        map<int, int> save;
+        for(int i=0;i<sz;i++){
+            save[intervals[i][0]]=i+1;
         }
-        return {-1,-1,-1};
-    }
-    static bool cmp(vector<int> a,vector<int> b){
-        return a[0]<b[0];
-    }
-
-    vector<int> findRightInterval(vector<vector<int>> &intervals) {// time O(n*log(n)), memmory O(n)
-
-        vector<vector<int>>temp=intervals;
-        vector<int> result;
-        unordered_map<int,int> interval_idx;
-        for(int i=0;i<(int) intervals.size();i++){
-            interval_idx[intervals[i][0]]=i;
-        }
-        sort(intervals.begin(),intervals.end(),cmp);
-        for(int i=0;i<(int) temp.size();i++) {
-            vector<int> found= search(intervals,temp[i][1]);
-            if(found.size()==3)
-            result.push_back(-1);
-            else result.push_back(interval_idx[found[0]]);
+        
+        for(int i=0;i<sz;i++){
+            result[i]=save.lower_bound(intervals[i][1])->second-1;
+            
         }
         return result;
 
