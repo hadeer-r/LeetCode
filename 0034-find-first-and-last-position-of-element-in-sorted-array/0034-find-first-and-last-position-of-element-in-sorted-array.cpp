ceb3f4230bab46b37471e53ea30a91140bd28f45
Hadeer Ramadan <147225434+hadeer-r@users.noreply.github.com>
2024-07-18 08:35:19 +0300

    Time: 4 ms (65.38%), Space: 16.8 MB (5.42%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,39 +1,7 @@
 class Solution {
 public:
-    int find_first(vector<int> &v, int target) {
-        int left = 0, right = (int)v.size() - 1;
-        while (left <= right) {
-            int middle = left + (right - left) / 2;
-            if (v[middle] == target) {
-                if (middle == 0 || v[middle - 1] != target)
-                    return middle;
-                else
-                    right = middle - 1;
-            } else if (target > v[middle])
-                left = middle + 1;
-            else if (target < v[middle])
-                right = middle - 1;
-        }
-        return -1;
-    }
-    int find_last(vector<int> &v, int target) {
-        int left = 0, right = (int)v.size() - 1;
-        while (left <= right) {
-            int middle = left + (right - left) / 2;
-            if (v[middle] == target) {
-                if (middle == (int)v.size() - 1 || v[middle + 1] != target)
-                    return middle;
-                else
-                    left = middle + 1;
-            } else if (target > v[middle])
-                left = middle + 1;
-            else if (target < v[middle])
-                right = middle - 1;
-        }
-        return -1;
-    }
-    vector<int> searchRange(vector<int>& nums, int target) {
-        if(nums.size()==0)
+
+    vector<int> searchRange(vector<int> &nums, int target) {
         if (nums.size() == 1) {
             if (nums[0] == target)
                 return {0, 0};
@@ -41,6 +9,12 @@ public:
                 return {-1, -1};
         }
         sort(nums.begin(), nums.end());
-        return {find_first(nums, target), find_last(nums, target)};
+        auto it = lower_bound(nums.begin(),nums.end(),target);
+        if(it==nums.end()||(*it!=target))
+            return {-1,-1};
+        int idx1=it- nums.begin();
+        auto it1= upper_bound(nums.begin(),nums.end(),target);
+        int idx2=it1-nums.begin()-1;
+        return {idx1,idx2};
     }
 };
