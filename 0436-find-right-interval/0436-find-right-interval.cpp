0398c7ede05d86794b98352bab83eea2af336daf
Hadeer Ramadan <147225434+hadeer-r@users.noreply.github.com>
2024-07-18 08:48:22 +0300

    Time: 33 ms (98.71%), Space: 27.9 MB (88.51%) - LeetHub

--- 0436-find-right-interval/0436-find-right-interval.cpp
+++ 0436-find-right-interval/0436-find-right-interval.cpp
@@ -1,18 +1,37 @@
 class Solution {
 public:
-    vector<int> findRightInterval(vector<vector<int>> &intervals) {
-        int sz=(int) intervals.size();
-        vector<int> result(sz);
-        map<int, int> save;
-        for(int i=0;i<sz;i++){
-            save[intervals[i][0]]=i+1;
+    static bool cmp(pair<int,int> a,pair<int,int> b){
+        return a.second<b.second;
+    }
+    int search(vector<pair<int,int>> &v,int target){
+        int start=0,end=(int)v.size()-1;
+        pair<int,int> res{-1,INT_MAX};
+        while (start<=end){
+            int middle=start+(end-start)/2;
+            if(v[middle].second==target)
+                return v[middle].first;
+            if(v[middle].second>target){
+                if(res.second>v[middle].second)
+                    res={v[middle].first,v[middle].second};
+                end =middle-1;
+            }
+            else
+                start=middle+1;
+
         }
-        
+        return res.first;
+    }
+    vector<int> findRightInterval(vector<vector<int>> &intervals) {
+        int sz=(int)intervals.size();
+        vector<int> result(sz);
+        vector<pair<int,int>> save(sz);
         for(int i=0;i<sz;i++){
-            result[i]=save.lower_bound(intervals[i][1])->second-1;
-            
+            save[i]={i,intervals[i][0]};
+        }
+        sort(save.begin(),save.end(),cmp);
+        for(int i=0;i<sz;i++){
+            result[i]= search(save,intervals[i][1]);
         }
         return result;
-
     }
 };
