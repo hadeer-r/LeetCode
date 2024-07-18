ef2119a286a7983f57ed98ffaa69a3ffa284cb62
Hadeer Ramadan <147225434+hadeer-r@users.noreply.github.com>
2024-07-18 10:47:52 +0300

    Time: 0 ms (100%), Space: 8.6 MB (6.76%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    long long sum(long long n){
+        long long c=n*((n+1)/2);
+        long long c2=(n/2)*(n+1);
+        if(c>c2)
+        return c;
+        return c2;
+    }
+    int arrangeCoins(int n) {
+        long long start=1,end=65535;
+        int res{0};
+        while(start<=end){
+            int middle=start+(end-start)/2;
+            long long calc=sum(middle);
+            long long calc2=sum(middle+1);
+            if(calc<=n)
+            {
+                res=middle;
+                start=middle+1;
+            }
+
+            else
+            end= middle -1;
+
+        }
+        return res;
+    }
+};
