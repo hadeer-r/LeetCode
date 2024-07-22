63451fde9207b5787979031f869240146e775843
Hadeer Ramadan <147225434+hadeer-r@users.noreply.github.com>
2024-07-22 23:10:00 +0300

    Time: 64 ms (19.56%), Space: 34.6 MB (5.87%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.cpp
+++ 2502-sort-the-people/2502-sort-the-people.cpp
@@ -1,16 +1,38 @@
+// class Solution {
+// public:
+//     vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+//         if ((int)names.size() == 1)
+//             return names;
+//         for (int i = 1; i < (int)names.size(); i++) {
+//             for (int o = i; o > 0; o--) {
+//                 if (heights[o] > heights[o - 1]) {
+//                     swap(heights[o], heights[o - 1]);
+//                     swap(names[o], names[o - 1]);
+//                 } else
+//                     break;
+//             }
+//         }
+//         return names;
+//     }
+// };
+
+// seconed solution
 class Solution {
 public:
+    static bool cmp(pair<string,int> a,pair<string,int> b) {
+        return a.second>b.second;
+    }
     vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
-        if ((int)names.size() == 1)
+        int sz=(int)names.size();
+        if (sz == 1)
             return names;
-        for (int i = 1; i < (int)names.size(); i++) {
-            for (int o = i; o > 0; o--) {
-                if (heights[o] > heights[o - 1]) {
-                    swap(heights[o], heights[o - 1]);
-                    swap(names[o], names[o - 1]);
-                } else
-                    break;
-            }
+        vector<pair<string,int>> sorting(sz);
+        for(int i=0;i<sz;i++){
+            sorting[i]= {names[i],heights[i]};
+        }
+        sort(sorting.begin(),sorting.end(),cmp);
+        for(int i=0;i<sz;i++){
+            names[i]=sorting[i].first;
         }
         return names;
     }
