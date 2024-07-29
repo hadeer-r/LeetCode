c15accfb3760b25a2438d495e5b73becd1972687
Hadeer Ramadan <147225434+hadeer-r@users.noreply.github.com>
2024-07-29 23:40:18 +0300

    Time: 0 ms (100%), Space: 10.8 MB (11.9%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        if((int)nums.size()==0) return 0;
+        int cnt{};
+        // for(auto x : nums) if(x!=val)cnt++;
+        int p1=0,p2=(int)nums.size()-1;
+        while(p1<p2){
+            if(nums[p2]==val){
+                p2--;
+                continue;
+            }
+            
+            if(nums[p1]==val){
+                swap(nums[p1],nums[p2]);
+                p2--;
+                p1++;
+                cnt++;
+            }
+            else
+            {p1++;cnt++;}
+
+        }
+        if(nums[p1]!=val) cnt++;
+        return cnt;
+
+    }
+};
