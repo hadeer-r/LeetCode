e7613c6398fafdc4e6fc307b82c07b31b86d021d
Hadeer Ramadan <147225434+hadeer-r@users.noreply.github.com>
2024-08-01 18:07:44 +0300

    Time: 290 ms (39.4%), Space: 168.6 MB (39.93%) - LeetHub

--- 2573-remove-nodes-from-linked-list/2573-remove-nodes-from-linked-list.cpp
+++ 2573-remove-nodes-from-linked-list/2573-remove-nodes-from-linked-list.cpp
@@ -1,37 +1,26 @@
-class Solution
-{
+class Solution {
 public:
-    ListNode *removeNodes(ListNode *head)
-    {
-        stack<ListNode *> nodes;
-        for (ListNode *cur = head; cur; cur = cur->next)
-        {
-            if (!nodes.empty() && cur->val > nodes.top()->val)
-            {
-                while (!nodes.empty())
-                {
-                    ListNode *tmp = nodes.top();
+    ListNode* removeNodes(ListNode* head) {
+        stack<ListNode*> nodes;
+        for (ListNode* cur = head; cur; cur = cur->next) {
+            if (!nodes.empty() && cur->val > nodes.top()->val) {
+                while (!nodes.empty()) {
+                    ListNode* tmp = nodes.top();
                     tmp->next = cur;
-                    if (cur->val > tmp->val)
-                    {
+                    if (cur->val > tmp->val) {
 
                         nodes.pop();
 
-                    }
-                    else
+                    } else
                         break;
                 }
+                if (nodes.empty())
+                    head = cur;
             }
-            if (nodes.size() == 1 && nodes.top()->val < cur->val)
-                nodes.pop();
 
             nodes.push(cur);
         }
-        while (!nodes.empty()){
-            head=nodes.top();
-            nodes.pop();
-        }
+
         return head;
-        
     }
 };
