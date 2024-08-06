bcf97a80af65720580d75f3bab5324fab622bb50
Hadeer Ramadan <147225434+hadeer-r@users.noreply.github.com>
2024-08-06 18:16:04 +0300

    Time: 0 ms (100%), Space: 7.5 MB (31.23%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
-    
-    bool isPowerOfTwo(int n) {// 
-        int start=0,end=31;
-        while(start<=end){
-            int middle=start+(end -start)/2;
-            long long power = pow(2,middle);
-            if(power==n)
+    bool isPowerOfTwo(int n) {
+        if (n == 1)
             return true;
-            if(power<n)
-            start=middle+1;
-            else
-            end=middle-1;
+        if (!n)
+            return false;
+        while (n != 1) {
+            if (n % 2 != 0)
+                return false;
+            n /= 2;
         }
-        return false;
+        return true;
     }
 };
