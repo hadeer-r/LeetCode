ce49bf105a474ee05749e10898bcf5146ca70aac
Hadeer Ramadan <147225434+hadeer-r@users.noreply.github.com>
2024-08-31 23:22:54 +0300

    Time: 12 ms (33.04%), Space: 10.9 MB (21.14%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -10,7 +10,6 @@ class Solution {
 public:
     bool hasCycle(ListNode *head) {
         if(!head || !head->next) return false;
-        unordered_map<ListNode*,bool> apperance;
         head->val =1e6;
         for(ListNode*cur=head;cur;cur=cur->next){
             if (cur->next&&cur->next->val == 1e6) return true;
