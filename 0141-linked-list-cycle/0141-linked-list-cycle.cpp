eb41e001cf92c048f5ec023da3393a13e440024e
Hadeer Ramadan <147225434+hadeer-r@users.noreply.github.com>
2024-08-31 23:37:46 +0300

    Time: 11 ms (44.95%), Space: 10.8 MB (60.3%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -8,14 +8,17 @@
  */
 class Solution {
 public:
-    bool hasCycle(ListNode *head) {
-        if(!head || !head->next) return false;
-        head->val =1e6;
-        for(ListNode*cur=head;cur;cur=cur->next){
-            if (cur->next&&cur->next->val == 1e6) return true;
-            else if(cur->next)
-            cur->next->val = 1e6;
+    bool hasCycle(ListNode* head) {
+        if (!head || !head->next)
+            return false;
 
+        (head->val < 0) ? head->val -= 1e6 : head->val += 1e6;
+        for (ListNode* cur = head; cur; cur = cur->next) {
+            if (cur->next && (cur->next->val >= 1e6 || cur->next->val <= -1e6))
+                return true;
+            else if (cur->next)
+                (cur->next->val < 0) ? cur->next->val -= 1e6
+                                     : cur->next->val += 1e6;
         }
         return false;
     }
