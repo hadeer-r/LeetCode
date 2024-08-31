7cbfe0707cedd005a361cf1dc143f72e4d4112ad
Hadeer Ramadan <147225434+hadeer-r@users.noreply.github.com>
2024-08-31 20:30:32 +0300

    Time: 3203 ms (41.2%), Space: 0B (100%) - LeetHub

--- 1773-percentage-of-users-attended-a-contest/1773-percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/1773-percentage-of-users-attended-a-contest.sql
@@ -1,7 +1,11 @@
 /* Write your T-SQL query statement below */
+
+declare @total_user float;
+select @total_user = count(*) from Users;
+
 select  R.contest_id, Round(
     (
-        (Cast (count(R.contest_id) as float) / Cast ((select count(*) from Users) As float))*100.0
+        (Cast (count(R.contest_id) as float) / @total_user)*100.0
     ),2
 ) As percentage
 from Users as U
