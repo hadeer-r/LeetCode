eca540189af30fa316aedc7c444222966ec78547
Hadeer Ramadan <147225434+hadeer-r@users.noreply.github.com>
2024-09-12 06:54:37 +0300

    Time: 30 ms (7.45%), Space: 28.1 MB (5.2%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        unordered_map<int, bool> numsMap;
+        int n = (int)nums.size();
+        for (auto x : nums) {
+
+            numsMap[x] = true;
+        }
+        for(int i=0;i<=n;i++){
+            if(numsMap[i]==false)
+            return i;
+        }
+        return -1;
+    }
+};
