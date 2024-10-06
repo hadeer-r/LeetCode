b77bd0e7d5d1b5feedc4ae1620f88e2d7c682e47
Hadeer Ramadan <147225434+hadeer-r@users.noreply.github.com>
2024-10-06 13:06:31 +0300

    Time: 25 ms (20.18%), Space: 36 MB (15.18%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int sz = nums.size();
+        vector<int> prefix(sz);
+        vector<int> postfix(sz);
+        prefix[0]=nums[0];
+        postfix[sz-1]=nums[sz-1];
+
+        // get prefix sum
+        for(int i=1;i<nums.size();i++){
+            prefix[i]= prefix[i-1]+nums[i];
+        }
+        //get postfix sum
+        for(int i=sz-2;i>=0;i--){
+            postfix[i]=postfix[i+1]+nums[i];
+        }
+
+        for(int i=0;i<sz;i++){
+            if(postfix[i]==prefix[i])
+            return i;
+        }
+        
+        return -1; 
+    }
+};
