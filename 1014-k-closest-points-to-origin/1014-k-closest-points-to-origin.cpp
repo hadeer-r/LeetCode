f85435302d798911b24938bbc123c4ef7a2d8912
Hadeer Ramadan <147225434+hadeer-r@users.noreply.github.com>
2025-06-18 22:49:23 +0300

    Time: 541 ms (5.63%), Space: 188.7 MB (5.01%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
@@ -2,7 +2,7 @@ class Solution {
 private:
     struct comp {
         bool operator()(pair<double,vector<int>> p1, pair<double,vector<int>> p2)const {
-            return p1.first < p2.first;
+            return p1.first > p2.first;
         }
     };
 public:
@@ -10,18 +10,19 @@ public:
     
     
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        multiset<pair<double,vector<int>>, comp> pp;
+        priority_queue<pair<double,vector<int>>, vector<pair<double,vector<int>>>, comp> pp;
         vector<vector<int>> result;
         for(int i=0;i<points.size();i++){
             double distance = sqrt(pow(points[i][0],2)+pow(points[i][1],2));
-            pp.insert({distance,points[i]});
+            pp.push({distance,points[i]});
         }
         int i=0;
-        for(auto it = pp.begin();it!=pp.end();it++){
-            
-            if(i==k) break;
-            result.push_back(it->second);
+        while(!pp.empty()){
+            if(i==k)break;
             i++;
+
+            result.push_back(pp.top().second);
+            pp.pop();
         }
         return result;
     }
