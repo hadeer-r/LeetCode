e2698b73e8780c2b15e46533aeda8cb5d73df8de
Hadeer Ramadan <147225434+hadeer-r@users.noreply.github.com>
2025-09-15 16:44:11 +0300

    Time: 0 ms (100%), Space: 9.8 MB (19.24%) - LeetHub

--- 1426-find-n-unique-integers-sum-up-to-zero/1426-find-n-unique-integers-sum-up-to-zero.cpp
+++ 1426-find-n-unique-integers-sum-up-to-zero/1426-find-n-unique-integers-sum-up-to-zero.cpp
@@ -6,16 +6,13 @@ public:
             result.push_back(0);
             return result;
         }
-        int side = n/2;
 
-        int currentNum = 1;
         if(n&1){
             result.push_back(0);
         }
-        for(int i = 0;i<n/2;i++){
-            result.push_back(currentNum*-1);
-            result.push_back(currentNum);
-            currentNum++;
+        for(int i = 1;i<=n/2;i++){
+            result.push_back(i*-1);
+            result.push_back(i);
         }
 
         return result;
