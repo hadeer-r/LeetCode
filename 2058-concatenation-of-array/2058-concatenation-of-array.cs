38de80fcb537bbf22b9e1f3c042738468ca37ec4
Hadeer Ramadan <147225434+hadeer-r@users.noreply.github.com>
2025-11-15 09:43:27 +0000

    Time: 0 ms (100%), Space: 52 MB (48.27%) - LeetHub

--- 2058-concatenation-of-array/2058-concatenation-of-array.cs
+++ 2058-concatenation-of-array/2058-concatenation-of-array.cs
@@ -2,12 +2,9 @@ public class Solution {
     public int[] GetConcatenation(int[] nums) {
         int n = nums.Length;
         int[] ans = new int[2*n];
-        for(int i=0;i<n;i++){
-            ans[i]=nums[i];
-        }
-        for(int i=n;i<2*n;i++){
-            ans[i]=nums[i-n];
-        }
+        nums.CopyTo(ans,0);
+        nums.CopyTo(ans,n);
+
         return ans;
     }
 }
