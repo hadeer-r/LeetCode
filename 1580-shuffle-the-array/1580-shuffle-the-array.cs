64691e467fa13f8db806815e3d75c19365018236
Hadeer Ramadan <147225434+hadeer-r@users.noreply.github.com>
2025-11-15 10:02:56 +0000

    Time: 136 ms (32%), Space: 49.6 MB (28.89%) - LeetHub

--- 1580-shuffle-the-array/1580-shuffle-the-array.cs
+++ 1580-shuffle-the-array/1580-shuffle-the-array.cs
@@ -1,14 +1,11 @@
 public class Solution {
     public int[] Shuffle(int[] nums, int n) {
         int[] ans = new int[n*2];
-        for(int i=0,j=0; i<2*n; i+=2){
-            ans[i]=nums[j];
-            ++j;
+        for(int i=0; i<n; i++){
+            ans[i*2] = nums[i];
+            ans[i*2+1]=nums[i+n];
         }
-        for(int i =1,j=n;i<n*2;i+=2){
-            ans[i]=nums[j];
-            ++j;
-        }
+        
         return ans;
     }
 }
