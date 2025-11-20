0a85a8560a632041e3f00ede351a3d7ff9767bf8
Hadeer Ramadan <147225434+hadeer-r@users.noreply.github.com>
2025-11-20 14:17:35 +0000

    Time: 11 ms (18.13%), Space: 48 MB (74.92%) - LeetHub

--- 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.cs
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.cs
@@ -1,33 +1,23 @@
 
 public class Solution {
     public int[] SmallerNumbersThanCurrent(int[] nums) {
-        SortedDictionary<int,List<int>> sortedWithIndexes = new SortedDictionary<int,List<int>>();
-        int n=nums.Length;
+        int n = nums.Length;
         int[] result = new int[n];
-        for(int i=0;i<n;i++){
-            if(sortedWithIndexes.ContainsKey(nums[i]*-1)){
-                sortedWithIndexes[nums[i]*-1].Add(i);
+        for (int i =0 ;i<n;i++){
+            if(i>0 && nums[i]==nums[i]-1){
+                result[i] = result [i-1];
             }
             else {
-                List<int> index = new List<int>();
-                index.Add(i);
-                sortedWithIndexes.Add(nums[i]*-1,index);
+                int cnt=0;
+                for(int j=0 ;j<n;j++){
+                    if(j==i) continue;
+                    if(nums[i]>nums[j]) cnt++;
+                }
+                result[i]=cnt;
             }
-        }
-        
-        foreach(KeyValuePair<int,List<int>> ele in sortedWithIndexes){
-            foreach(var x in ele.Value){
-                result[x]=n-ele.Value.Count;
-            }
-            n = n - ele.Value.Count;
+
         }
         return result;
     }
     
-}
-public class NumsComp: IComparer<KeyValuePair<int,List<int>>> {
-
-        public int Compare(KeyValuePair<int,List<int>> a, KeyValuePair<int,List<int>> b){
-            return (a.Key).CompareTo(b.Key);
-        }
 }
